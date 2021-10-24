0137ffcb66a792b1a16cd1bdb8b74ac3e90cbbbe
fmroque10 <40742859+fmroque10@users.noreply.github.com>
2021-10-24 12:49:10 -0600

    Add files via upload

--- kmeans3dplot01.r
+++ kmeans3dplot01.r
@@ -1,11 +1,8 @@
-
-
-
   
 #install.packages("scatterplot3d")   
 library("scatterplot3d")
 
-rng <- EXCEL$Application$get_Range( "B2:D35" )
+rng <- EXCEL$Application$get_Range( "E2:G38073" )
 X <- rng$get_Value()
 
 
